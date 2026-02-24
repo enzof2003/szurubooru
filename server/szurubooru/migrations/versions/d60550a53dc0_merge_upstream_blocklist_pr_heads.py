3f1308cda4c8f6e60e1f6f42bc5a82e744875155
enzof2003 <enzof2003@outlook.fr>
2026-02-24 03:38:11 +0100

    Final Blocklist feature implementation + alembic header merge

---
+++ server/szurubooru/migrations/versions/d60550a53dc0_merge_upstream_blocklist_pr_heads.py
@@ -0,0 +1,22 @@
+'''
+Merge upstream + blocklist PR heads
+
+Revision ID: d60550a53dc0
+Created at: 2026-02-24 02:29:20.618302
+'''
+
+import sqlalchemy as sa
+from alembic import op
+
+
+
+revision = 'd60550a53dc0'
+down_revision = ('5b5c940b4e78', '9ba5e3a6ee7c')
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
